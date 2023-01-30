584b502b9e8be0806e74f14d315be819ac939c82
clararingrose <91962686+clararingrose@users.noreply.github.com>
2023-01-30 16:32:21 +0000

    Update PowerExperiments.java

--- src/PowerExperiments.java
+++ src/PowerExperiments.java
@@ -1,6 +1,9 @@
 public class PowerExperiments {
+
+    static private int count = 0;
+
     public static void main(String[] args) throws Exception {
-        simplePowerExperiments();
+        smartPowerExperiments();
     }
 
     static double power(double x, int n){
@@ -8,12 +11,46 @@ public class PowerExperiments {
 
         for(int i = 1; i <= n; i++){
             p *= x;
+            count++;
         }
 
         return p;
     }
 
     static void simplePowerExperiments(){
-        
+        System.out.println("n,count");
+
+        for(int i = 0; i < 20; i++){
+            count = 0;
+            power(2,i);
+            System.out.println(i + "," + count);
+        }
+    }
+
+    static double smartPower(double x, int n){
+        double p = 1;
+        double q = x;
+        int m = n;
+
+        while(m > 0){
+            if(m % 2 != 0){
+                p = p * q;
+            }
+            m = (int)Math.floor(m/2);
+            q = q * q; 
+            count++; 
+        }
+
+        return(p);
+    }
+
+    static void smartPowerExperiments(){
+        System.out.println("n,count");
+
+        for(int i = 0; i < 20; i++){
+            count = 0;
+            smartPower(2,i);
+            System.out.println(i + "," + count);
+        }
     }
 }
